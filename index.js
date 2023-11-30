efe21ce5c246495b26bbea711ae96981a83d56c2
unknown <Admin@FDLW190004.corp.mahindra.com>
2023-11-30 14:18:45 +0530

    count add

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+// timer
+
+const startingMinutes = 72000000;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById("countdown");
+
+// counter div
 const counterDiv = document.getElementById("counterDiv");
+
+// setInterval(updateCountdown, 1000);
+
+// function updateCountdown() {
+//   const minutes = Math.floor(time / 60);
+//   let seconds = time % 60;
+
+//   seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//   counterDiv.innerHTML = `${minutes} ${seconds}`;
+//   time++;
+// }
 
 const counterNumber = [
   {
@@ -39,63 +59,87 @@ const firstFourDiv = document.getElementById("firstFour");
 const secondFourDiv = document.getElementById("secondFour");
 
 const firstFour = [
-    {
-        count:12000,
-        text:"Actual"
-    },
-    {
-        count:11000,
-        text:"Actual"
-    },
-    {
-        count:1000,
-        text:"Actual"
-    },
-    {
-        count:18000,
-        text:"Actual"
-    },
-]
+  {
+    count: 12000,
+    text: "Actual",
+  },
+  {
+    count: 11000,
+    text: "Actual",
+  },
+  {
+    count: 1000,
+    text: "Actual",
+  },
+  {
+    count: 18000,
+    text: "Actual",
+  },
+];
 
 const secondFour = [
-    {
-        count:50000,
-        text:"Planned"
-    },
-    {
-        count:90000,
-        text:"Planned"
-    },
-    {
-        count:60000,
-        text:"Planned"
-    },
-    {
-        count:20000,
-        text:"Planned"
-    },
-]
+  {
+    count: 50000,
+    text: "Planned",
+  },
+  {
+    count: 90000,
+    text: "Planned",
+  },
+  {
+    count: 60000,
+    text: "Planned",
+  },
+  {
+    count: 20000,
+    text: "Planned",
+  },
+];
 
 // first four
 firstFour.forEach((item) => {
-    const p = document.createElement("p");
-    p.textContent = item.count;
-    const span = document.createElement("span");
-    span.textContent = item.text;
+  const p = document.createElement("p");
+  p.textContent = item.count;
+  const span = document.createElement("span");
+  span.textContent = item.text;
 
-    p.appendChild(span);
+  p.appendChild(span);
 
-    firstFourDiv.appendChild(p)
-})
+  firstFourDiv.appendChild(p);
+});
 
 // second four
 secondFour.forEach((item) => {
-    const p = document.createElement("p");
-    p.textContent = item.count;
-    const span = document.createElement("span");
-    span.textContent = item.text;
+  const p = document.createElement("p");
+  p.textContent = item.count;
+  const span = document.createElement("span");
+  span.textContent = item.text;
 
-    p.appendChild(span);
+  p.appendChild(span);
 
-    secondFourDiv.appendChild(p)
-})
+  secondFourDiv.appendChild(p);
+});
+
+// ========================================
+// let exampleNumber = 72000;
+
+// function incrementLastDigit() {
+//   // Convert the number to a string to easily access individual digits
+//   const numberString = exampleNumber.toString();
+
+//   // Convert the string to an array of digits
+//   const digitsArray = numberString.split('').map(Number);
+
+//   // Increment the last digit by 1
+//   digitsArray[digitsArray.length - 1] += 1;
+
+//   // Join the array back into a string with spaces between the digits
+//   const result = digitsArray.join(' ');
+
+//   // Update the example number and log the result
+//   exampleNumber = parseInt(result.replace(/\s/g, ''), 10);
+//   console.log(result);
+// }
+
+// // Set up the interval to call the incrementLastDigit function every 1000 milliseconds (1 second)
+// setInterval(incrementLastDigit, 1000);
